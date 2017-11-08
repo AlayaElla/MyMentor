da6c8871b9dba2ada0bfc959599ffbaed62ea37d
AlayaElla <515451867@qq.com>
2017-11-09 02:13:30 +0800

    增加播放故事脚本的工具，同时追加一些代码工具类和临时资源

---
+++ Assets/Script/Chat/ChatSystemManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ChatSystemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+    }
+
+    ChatSystemTool chatmanager;
+    ArrayList StoryList = new ArrayList();
+
+    public void StartStory()
+    {
+        if (StoryList.Count == 0 || StoryList == null)
+        {
+            Debug.LogError("Can't Start Story,Don't have StoryList!");
+            return;
+        }
+
+        if (chatmanager == null)
+        {
+            GameObject newobj = new GameObject();
+            newobj.name = "ChatSystem";
+            chatmanager = newobj.AddComponent<ChatSystemTool>();
+            newobj.AddComponent<AudioSource>();
+        }
+        chatmanager.PushStoryList(StoryList);
+        string storyname = (string)StoryList[0];
+
+        Loading.GetInstance().LoadingStoryScene(storyname, () =>
+        {
+            chatmanager.LoadChatStory(storyname);
+        });
+    }
+
+    public void StartStory(string storyname)
+    {
+        AddStroyName(storyname);
+        StartStory();
+    }
+
+    void AddStroyName(string storyname)
+    {
+        if(!StoryList.Contains(storyname))
+            StoryList.Add(storyname);
+    }
+}
---
+++ Assets/Script/Tools/XmlTool.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Xml;
+using System.Text;
+using System;
+
+public class XmlTool
+{
+    //读取文本配置表
+    public ArrayList loadChatConfigXmlToArray()
+    {
+        //保存路径
+        string filepath = "Config/Story/ChatConfig";
+
+        string _result = Resources.Load(filepath).ToString();
+
+        ArrayList ChatConfig = new ArrayList();
+
+        XmlDocument xmlDoc = new XmlDocument();
+
+        xmlDoc.LoadXml(_result);
+
+        XmlNodeList nodeList = xmlDoc.SelectSingleNode("ChatConfig").ChildNodes;
+
+        foreach (XmlElement config in nodeList)
+        {
+            ChatSystemTool.ChatConfig _chatconfig = new ChatSystemTool.ChatConfig();
+
+            //读取node内属性，把string转化为对应的属性
+            if (config.GetAttribute("Languege") != "")
+                _chatconfig.Languege = config.GetAttribute("Languege");
+            if (config.GetAttribute("Speed") != "")
+                _chatconfig.speed = float.Parse(config.GetAttribute("Speed"));
+            //添加进itemList中
+            ChatConfig.Add(_chatconfig);
+        }
+        return ChatConfig;
+    }
+  
+    //获取路径//
+    private static string GetDataPath()
+    {
+        return Application.dataPath + "/Resources";
+    }
+}
